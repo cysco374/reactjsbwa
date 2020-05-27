ffd0d3fe336ac23e2649cff76b1f91add5d486ad
cysco374 <cysco374@yahoo.co.id>
2020-05-27 10:56:46 +0700

    Add Key

--- src/ProdukInfo.js
+++ src/ProdukInfo.js
@@ -4,28 +4,30 @@ import './App.css';
 // import TambahCart from './App';
 import CheckDiscount from './Discount';
 import TambahCart from './Cart';
+import PropTypes from 'prop-types';
 
 function ProdukInfo(props) {
     const { category, name, isDiscount } = props;
     const benefits = ["Buah segar", "Kualitas terbaik", "Besar"];
-    const listBenefits = benefits.map((itemBenefits) => <li>{itemBenefits}</li>)
+    const listBenefits = benefits.map((itemBenefits) =>
+        <li key={itemBenefits}>{itemBenefits}</li>)
     return (
-      <div>
-        <div className="Deskripsi">
-          <p className="Category">{category}</p>
-          <h1 className="Title">{name}</h1>
-          <p className="Price">IDR 11.000</p>
-          <CheckDiscount isDiscount={isDiscount} />
-          <p className="Info">
-            Buah pepaya matang dan sehat, dengan harga terjangkau. Tersedia untuk area DIY dan sekitarnya.
+        <div>
+            <div className="Deskripsi">
+                <p className="Category">{category}</p>
+                <h1 className="Title">{name}</h1>
+                <p className="Price">IDR 11.000</p>
+                <CheckDiscount isDiscount={isDiscount} />
+                <p className="Info">
+                    Buah pepaya matang dan sehat, dengan harga terjangkau. Tersedia untuk area DIY dan sekitarnya.
           </p>
-          <ul>
-            {listBenefits}
-          </ul>
-          <a onClick={(i) => TambahCart(name, i)} href="#">Add to Cart</a>
+                <ul>
+                    {listBenefits}
+                </ul>
+                <a onClick={(i) => TambahCart(name, i)} href="#">Add to Cart</a>
+            </div>
         </div>
-      </div>
     );
-  }
+}
 
-  export default ProdukInfo;
+export default ProdukInfo;
--- src/Reviews.js
+++ src/Reviews.js
@@ -24,7 +24,7 @@ function Reviews() {
     }
     ];
     const listReview = users.map((itemReview) =>
-        <div className="Item" >
+        <div key={itemReview.id} className="Item" >
             <img src={itemReview.fotoReview} />
             <div className="User" >
                 <h3> {itemReview.name} </h3>
